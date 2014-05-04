d2b4e551e98cfb5c3443be9af150282d56b58748
Troy Shu <tmshu1@gmail.com>
2014-05-03 20:46:05 -0400

    concur calls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,25 @@ class StaticPagesController < ApplicationController
 
   def getExpenseEntries(responseReports)
   	#takes in a list of expense report json objects
-  	
+  	expenseEntryList = Array.new
+  	responseReports.each do |reportJson|
+  		#logger.debug("reportJSON: #{reportJson}")
+  		reportId = reportJson['ID']
+  		#logger.debug("reportId: #{reportId}")
+  		#https://www.concursolutions.com/api/expense/expensereport/v2.0/report/EBE35F97898140B1B2F5
+  		response = RestClient.get "https://www.concursolutions.com/api/expense/expensereport/v2.0/report/#{reportId}", { 'Authorization' => "Oauth MQHO89lqHVPrMtHTtLF3CAaf0Co=", "accept" => "application/json"}
+  		#logger.debug("repord entry response: #{response}")
+  		responseJson = JSON.parse(response)
+  		entries = responseJson['ExpenseEntriesList'].split(",")
+
+  		entries.each do |entry|
+  			#logger.debug("entry 0: #{entry[0]}")
+  			expenseEntryList.push(entry[0].to_s)
+  		end
+
+  		
+  	end
+  	return expenseEntryList
 
   end
 
@@ -27,7 +45,15 @@ class StaticPagesController < ApplicationController
 
 	responseReports = JSON.parse(response)["Items"]
 
-	expenseEntries = getExpenseEntries(responseReports)
+	expenseEntriesList = getExpenseEntries(responseReports)
+	expenseEntriesStr = expenseEntriesList.join(",")
+
+	#logger.debug("exense entries: #{expenseEntriesStr}")
+
+	@expenseEntries = "{\"entries\": [#{expenseEntriesStr}] }"
+
+
+	logger.debug(@expenseEntries)
 
 
   end
